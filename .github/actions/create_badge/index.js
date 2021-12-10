10af9686f4fbef2dd4d5bb3d4f4ec90c077a4ef5
JDiezGarcia <fco.javier.diez.garcia@gmail.com>
2021-12-10 21:15:32 +0100

    First

---
+++ .github/actions/create_badge/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const fs = require('fs');
+
+async function create_badge() {
+
+    let outcome = core.getInput('cypress');
+    let readme = 'README.md'; 
+    let badge;
+    if (outcome == 'success') {
+        badge = '[![badge-success](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)'
+    } else {
+        badge = '[![badge-failure](https://img.shields.io/badge/test-failure-red)]'
+    }
+
+    fs.readFile(readme, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/(?<=\<!---Start place for the badge --\>\n)[^]+(?=\n\<!---End place for the badge --\>)/g, badge);
+
+        fs.writeFile(readme, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+
+}
+
+create_badge();
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Example of nextjs project using Cypress.io
-
+RESULTADO DE LOS ÚLTIMOS TESTS:
 <!---Start place for the badge -->
 [![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)
 
