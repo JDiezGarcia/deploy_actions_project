a2fed6bc3fd175b906d9bfa8c77d7f0352af7f9b
JDiezGarcia <fco.javier.diez.garcia@gmail.com>
2021-12-10 22:24:27 +0100

    Second

---
+++ .github/actions/create_badge/dist/index.js
@@ -0,0 +1,93 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 224:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 147:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("fs");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(224);
+const fs = __nccwpck_require__(147);
+
+async function create_badge() {
+
+    let outcome = core.getInput('cypress');
+    let readme = 'README.md'; 
+    let badge;
+    if (outcome == 'success') {
+        badge = '[![badge-success](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)'
+    } else {
+        badge = '[![badge-failure](https://img.shields.io/badge/test-failure-red)]'
+    }
+
+    fs.readFile(readme, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var result = data.replace(/(?<=\<!---Start place for the badge --\>\n)[^]+(?=\n\<!---End place for the badge --\>)/g, badge);
+
+        fs.writeFile(readme, result, 'utf8', function (err) {
+            if (err) return console.log(err);
+        });
+    });
+
+}
+
+create_badge();
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
